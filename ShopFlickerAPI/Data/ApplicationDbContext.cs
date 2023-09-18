1f0435815e098c9dcb16b293e15b2d8ed8e7c4cd
001osama <osamanasim909@gmail.com>
2023-09-18 12:17:01 +0500

    Add project files.

---
+++ ShopFlickerAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopFlickerAPI.Models;
+
+namespace ShopFlickerAPI.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            :base(options)
+        {}
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ShopFlickerAPI/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopFlickerAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Amount{ get; set; }
+        public string Desc { get; set; }
+    }
+}
